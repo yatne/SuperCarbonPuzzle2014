8d4141e662987f2df8e77ceef4f2f1c7181ffc08
Antoni <antonimaciej@gmail.com>
2019-03-17 10:23:18 +0100

    fixed ad

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -102,8 +102,17 @@ public class AndroidLauncher extends AndroidApplication implements ActionResolve
 
     @Override
     public void showInterstitialAd() {
-        if (mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
+        runOnUiThread(new Runnable() {
+            @Override public void run() {
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                }
+            }
+        });
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
